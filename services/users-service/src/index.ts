53a93cf501fd16b5f758abb5c29043601bb0e0c2
Artem Tarasenko <shabashab.04@gmail.com>
2022-04-16 13:42:47 +0300

    feat(users-service): initialize basic express server

--- services/users-service/src/index.ts
+++ services/users-service/src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+import express from "express";
+import rootRouter from "./routes/";
+
+(async () => {
+  const isProduction = process.env.NODE_ENV === "production";
+
+  //Trying to load .env if in development
+  if(!isProduction) {
+    try {
+      const dotenv = await import("dotenv");
+      dotenv.config();
+    } catch (e) {
+      //Ignore
+    }
+  }
+
+  const PORT = process.env.APP_PORT || 3000;
+
+  const app = express();
+
+  app.use(rootRouter);
+
+  app.listen(PORT, () => {
+    console.log("Application successfully started on port 3000");
+  });
+})().then(() => {});
